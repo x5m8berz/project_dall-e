e0b6fda851ecb08d40593869fa6fa74ca5458648
Hanrocky <hanrocky.halim@gmail.com>
2023-02-08 00:03:14 +0700

    Finish setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,6 +17,12 @@ const App = () => {
           Create
         </Link>
       </header>
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9f8fe] min-h-[calc(100vh-73px)]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">
+          The Community Showcase
+        </h1>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
