04937be179a6271d65c6047dab5adf9d7913a598
Hanrocky <hanrocky.halim@gmail.com>
2023-02-15 15:33:31 +0700

    Configure server

--- server/index.js
+++ server/index.js
@@ -24,7 +24,9 @@ app.get("/", async (req, res) => {
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => console.log("Server started on port 8080"));
+    app.listen(`0.0.0.0:$PORT`, () =>
+      console.log("Server started on port 8080")
+    );
   } catch (error) {
     console.log(error);
   }
